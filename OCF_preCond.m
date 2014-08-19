b26c9e0864e2605ec577654aa039d741b378f6d7
youyuqi <youlilly@gmail.com>
2014-08-19 09:48:06 -0500

    -Added the fMRI version of SUDS at the end; ready for revision; look for “****:

--- OCF_preCond.m
+++ OCF_preCond.m
@@ -329,99 +329,172 @@ cgpencol(0,0,0) ;
 cgflip ;
 
 startwait = cogstd('sGetTime', -1) * 1000 ;
-while ((cogstd('sGetTime', -1) * 1000) < (startwait + 3000)) end %blank screen for 3 sec
+while ((cogstd('sGetTime', -1) * 1000) < (startwait + 3000)) 
+end %blank screen for 3 sec
 
 %SUDS 1 rating
-%Cursor image
-CursorImage = [0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 0 0 0; 0 0 0; 0 0 0; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 1 1 1; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 1 1 1; 0 0 0; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 0 0 0; 1 1 1; 1 1 1; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-               1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 0 0 0; 0 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; 1 0 0; ...
-              ];
-%
-% Load up the cursor into sprite 40
-%
-cgloadarray(20,16,21,CursorImage)
-cgtrncol(20,'r')
 
-    cgkeymap;
-    cgmouse;
-    bprevious=0;
-    k_previous(28) = 0;
-    c=0;
-    riskindex = 999;
+cgfont('Arial',36);
+cgpencol(0,0,0);
+
+%Which buttons have the function of increase, decrease, or finishing the
+%question
+r_inc = 2;
+r_dec = 1;
+r_sel = 4;
+but_resp=0;
+
+pause on;
+
+rating = 0;
+ok_trig = 0;
+keyStrings = {'b','y','g','r'};
+
+keyExists = 0;
+keyReg = 0;
+
+for i = 1
+    rating = 0; %The current position of the selection line
+    ok_trig = 0; %The key value for finishing the question
     
-    while ~k_previous(28)
-    [k_current, k_previous] = cgkeymap ;
-    [x2,y2,bs,bprevious]=cgmouse;
+    %Draw the question, rating line, current position, and instructions
+    cgrect(0, 0, ScrWid, ScrHgh, [1 1 1]);
+    cgtext('How distressed/anxious are you feeling now?',0, 100);
+    cgdraw([-200],[-50],[200],[-50]);
+    cgdraw([rating],[-40],[rating],[-60]);
+    cgdraw([-200 0 200],[-55 -55 -55],[-200 0 200],[-45 -45 -45])
     
-        if ~bprevious && c==0
-        cgrect(0, 0, ScrWid, ScrHgh, [1 1 1])
-        cgfont('Arial',25)
-        cgpencol(0,0,0) %white letters
-        cgtext('How distressed and anxious are you right now?', 0, 3* ScrHgh / 9 - 15);
-        cgtext('Click anywhere on the line and press ENTER to confirm',0,2*ScrHgh/9-15);
-        cgtext('Not anxious                                                   Extremely',0,-1 * ScrHgh/9 - 6);
-        cgtext('At all                                                           Anxious',0,-1.5 * ScrHgh/9 - 6);
-   
-        %Draw the cursor
-        cgalign('l','t')
-        cgdrawsprite(20, x2+2.5,y2-2)
-        cgalign('c','c')
+    cgtext('Moderately',0, -115);
+    cgtext('Not at all',-200, -115);
+    cgtext('Extremely',200, -115);
+    cgflip;
+    pause(.5); %The script waits half a second before allowing any inputs - ensures that early button presses aren't registered
+    FlushEvents
+    while ok_trig == 0
         
-        cgpenwid(3);
-        cgdraw (-250,0,250,0);
+        [keyIsDown,press_time,keyArr] = KbCheck(); %Check the button box for inputs
         
-        cgflip(1,1,1)
-  
-        elseif bprevious
-        cgrect(0, 0, ScrWid, ScrHgh, [1 1 1])
-        cgfont('Arial',25)
-        cgpencol(0,0,0) %white letters
-        cgtext('How distressed and anxious are you right now?', 0, 3* ScrHgh / 9 - 15);
-        cgtext('Click anywhere on the line and press ENTER to confirm',0,2*ScrHgh/9-15);
-        cgtext('Not anxious                                                   Extremely',0,-1 * ScrHgh/9 - 6);
-        cgtext('At all                                                           Anxious',0,-1.5 * ScrHgh/9 - 6);
-   
-        %Draw the cursor
-        cgalign('l','t')
-        cgdrawsprite(20, x2+2.5,y2-2)
-        cgalign('c','c')
+        %Everything from here on is nested in if loops.  This is so the
+        %only thing that is continuously being run is KbCheck.  There will
+        %be trigger values to move on to every subsequent loop.  This is
+        %not graceful but it should work.
         
-        cgpenwid(3);   
-        cgdraw (-250,0,250,0);
-        
-        cgpencol(1,0,0)
-        cgpenwid(4);
-        cgdraw (x2,y2-10,x2,y2+10);
-        cgflip
-        pause(1.5);
-        
-        SUDS1 = (x2+250)/500*100;
-        
-        bprevious = 0;
-        c = 0;
-        
+        if keyIsDown == 1 && keyExists == 0
+            %This just enusres that we're looking only for the correct keys
+            keyListCoded = zeros(size(keyArr));
+            if nargin<1 || isempty(keyStrings)
+                keyListCoded = 1-keyListCoded;
+            else
+                keyListCoded(KbName(keyStrings)) = 1;
+            end
+            
+            key = KbName(find(keyArr)); %Finds the identity of the keys that are pressed
+            keyExists = 1;
+        else
         end
-    
+        
+        %This loop only runs if the previous loop runs.  This ensures that
+        %everything properly runs sequentially (having keyIsDown here may
+        %not ensure this since it is the dependency of the previous loop as
+        %well).
+        if keyExists == 1 && keyReg==0
+           % if ~iscell(key) && key == 't' %Filters out TTL values when they are the only things there
+           %     key=[];
+            %    keyExists = 0;
+           % else
+               % resp_length=length(key); %If there are two simultaneous inputs, key will be a cell; this must be filtered.
+%                 for i=1:(resp_length+1)
+%                     keycell={'l'};
+%                     keycell(1,i+1)={key};
+                    button=key;
+                    if button=='b'
+                        but_resp=1;
+                        keyReg = 1; %The reason this is here is so it only reads the first correct button press.  Just in case there are two button presses that correspond to functional keys, rather than 't'.
+                    elseif button=='y'
+                        but_resp=2;
+                        keyReg = 1;
+                    elseif button=='r'
+                        but_resp=4;
+                        keyReg = 1;
+                    else
+                    end
+              %  end
+           % end
+        end
+        
+        %This loop only runs if one of the buttons above has been pressed.
+        %This is where the actual visual change takes place.
+        if keyReg == 1
+            if but_resp == r_dec
+                %First, registor the rating change.  The screen has to be
+                %redrawn - unfortunately it cannot be done in a simpler way
+                %at this time.
+                rating = rating - 8;
+                if rating < -200
+                    rating = -200;
+                end
+                cgrect(0, 0, ScrWid, ScrHgh, [1 1 1]);
+                cgtext('How distressed/anxious are you feeling now?',0, 100);
+                cgdraw([-200],[-50],[200],[-50]);
+                cgdraw([rating],[-40],[rating],[-60]);
+                cgdraw([-200 0 200],[-55 -55 -55],[-200 0 200],[-45 -45 -45])
+                
+                cgtext('Moderately',0, -115);
+                cgtext('Not at all',-200, -115);
+                cgtext('Extremely',200, -115);
+                cgflip;
+                
+                %Now clear things out.  Reset all trigger variables to
+                %their starting state and flush the inputs.  The pause
+                %makes sure that participants can move up and down in
+                %controlled increments - without the pause the marker would
+                %move too quickly.
+                FlushEvents;
+                but_resp=0;
+                keyExists = 0;
+                keyReg = 0;
+                pause(.05)
+            elseif but_resp == r_inc
+                rating = rating + 8;
+                if rating > 200
+                    rating = 200;
+                end
+                cgrect(0, 0, ScrWid, ScrHgh, [1 1 1]);
+                cgtext('How distressed/anxious are you feeling now?',0, 100);
+                cgdraw([-200],[-50],[200],[-50]);
+                cgdraw([rating],[-40],[rating],[-60]);
+                cgdraw([-200 0 200],[-55 -55 -55],[-200 0 200],[-45 -45 -45])
+                
+                cgtext('Moderately',0, -115);
+                cgtext('Not at all',-200, -115);
+                cgtext('Extremely',200, -115);
+                cgflip;
+                
+                FlushEvents;
+                but_resp=0;
+                keyExists = 0;
+                keyReg = 0;
+                pause(.05)
+                
+            elseif but_resp == r_sel
+                ok_trig = 1;
+                
+                %Put this here just in case this script needs to loop for
+                %other ratings.  It should have no impact on the single
+                %rating.
+                FlushEvents;
+                but_resp=0;
+                keyExists = 0;
+                keyReg = 0;
+            else
+                FlushEvents;
+            end
+            
+        end
     end
+end
+distressed = 50 + (rating / 4);
+%%%%%%end of rating
 
 cd C:\OCB\Data;
 eval(['save OCB_beh_precond_sub' num2str(subnum) '_' name_id ';']);
