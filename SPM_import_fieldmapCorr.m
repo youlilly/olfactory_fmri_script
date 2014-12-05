de0a9194f6ddd5e40b44f963962fefa234af9029
youyuqi <youlilly@gmail.com>
2014-12-05 13:45:05 -0600

    Updated to facilitate analysis on my local mac

--- SPM_import_fieldmapCorr.m
+++ SPM_import_fieldmapCorr.m
@@ -18,14 +18,15 @@
 
 %% Run this cell to set up initial parameters
 
-mainpath = '/study/ocf/mri/preprocess/Prep';
+%mainpath = '/study/ocf/mri/preprocess/Prep';
+mainpath = '/Volumes/Elements 1/OCF';
 epi1name = 'PreCond';
 epi2name = 'Cond';
 epi3name = 'PostCond';
 epi4name = 'PostCond2';
 epi5name = 'Olocalizer';
 epi6name = 'Resting';
-%epi7name = 'Olocalizer2';
+epi7name = 'PostCond2_p2';
 %epi7name = 'PostCond2_p2';
 % dum1name = 'Dummy1';
 % dum2name = 'Dummy2';
@@ -151,8 +152,8 @@ end
 
 %% dicom import EPI images
 
-subs = 28:31;%:30;
-mainpath = '/study/ocf/mri/preprocess/Prep';
+subs = 28;%:30;
+%mainpath = '/study/ocf/mri/preprocess/Prep';
 cd(mainpath);
 
 for i = 1:length(subs)
@@ -160,7 +161,7 @@ for i = 1:length(subs)
     fullfol = strcat(mainpath,'/',subfol); % fullfol = '/study/ocf/mri/preprocess/Prep/sub6'
     cd(fullfol);
     
-    for runs = 1:3
+    for runs = 7
         sfiles = [];
         
         %If you want to strcat path from local computer, use the backward
@@ -192,9 +193,10 @@ for i = 1:length(subs)
             outdir = strcat(fullfol,'/',epi6name);
             Filter = 'resting';
         else
-             dicomdir = strcat('/study1/ocf/mri/preprocess/0',num2str(subs(i)),'_2/Olocalizer2');
+            % dicomdir = strcat('/study1/ocf/mri/preprocess/0',num2str(subs(i)),'_2/Olocalizer2');
+            dicomdir = '/Volumes/Elements/Sub28_PostCond_2_unzipped/Postcond2';
             outdir = strcat(fullfol,'/',epi7name);
-            Filter = 'odor';        
+            Filter = 'postcond';        
         end
         
         
@@ -216,15 +218,15 @@ for i = 1:length(subs)
 end
 
 %%
-
-
 %% STEP 2: Slice-Time Correction
 % This step performs slice-time correction, which resolves the signal
 % values according to the time they were collected.  Because each image is
 % really a vision of the brain over the course of time of one TR (in our
 % case, 2.35 seconds), this step makes sure that the signal is accurate
 % according to the timing of collection.
-subs =[18 20];
+% 
+
+subs = 35:37;%:34;%[28:34]; %34];
 % These values are preset according to our needs.  If there is variation in
 % any of these, do not use the batch script for that participant; instead,
 % use the individual script.
@@ -246,7 +248,7 @@ timing(1) = TA / (nslices -1);
 sliord = [48:-2:2, 47:-2:1];
 refslice = 2;
 
-mainpath = '/study/ocf/mri/preprocess/Prep';
+%mainpath = '/study/ocf/mri/preprocess/Prep';
 cd(mainpath);
 
 for i = 1:length(subs)
@@ -307,7 +309,7 @@ end
 % These flags are the inputs to the realign and reslice commands.  They
 % should not vary between subjects; if they do, use the individual script
 % for that subject.
-subs = 13:16%:6;
+subs = 26:31%:34;%:6;
 
 estflag.quality = 0.9;
 estflag.sep = 4;
@@ -394,7 +396,7 @@ end
 % These flags are the inputs to the realign and reslice commands.  They
 % should not vary between subjects; if they do, use the individual script
 % for that subject.
-subs = 13%:6;
+subs = 9%:6;
 
 estflag.quality = 0.9;
 estflag.sep = 4;
@@ -474,7 +476,7 @@ spm_reslice(rfiles,resflag);
 end
 
 
-%% STEP: Realignment - Session2 for sub1&2
+%% STEP 3b: Realignment - Session2 for sub1&2
 % This step aligns all the EPI files so that they don't vary in position
 % from one image to another.  This is vital for proper analysis and also
 % gives us the motion parameters for the model.
@@ -577,7 +579,126 @@ spm_realign(rfiles,estflag);
 spm_reslice(rfiles,resflag);
 end
 
-%% STEP 5: Prepare Fieldmaps
+%% STEP 4: Censor List
+% Run a loop to calculate every volume motion; cutoff 2mm
+% This finds points where the motion from one image to another is large
+% enough that it can potentially create artifact.  A list is made of images
+% where this applies.  This precrocessing scrpit ensures that all relevant
+% images are put in the censor list, including images surronding the
+% aberrant volumes.
+
+% for i = 1:length(subs)
+%     subfol = strcat('sub',num2str(subs(i)));
+%     fullfol = strcat(mainpath,subfol);
+% for run = 1:2
+%     % Import the mation parameters from the text file output by SPM after
+%     % realignment.
+%     if run == 1
+%         rpfile = strcat(fullfol,'\',epi1name,'\rp_asRMRRFSWT001-0004-00001-000336-01');
+%     else
+%         rpfile = strcat(fullfol,'\',epi2name,'\rp_asRMRRFSWT001-0008-00001-000336-01');
+%     end
+% rps = importdata(rpfile);
+% sub = [2 3 5:18];
+sub = 4;
+mainpath = '/Volumes/Elements/OCF';
+cd(mainpath);
+
+for i = 1:length(sub)
+    subfol = strcat('sub',num2str(sub(i)));
+    fullfol = strcat(mainpath,'/',subfol);
+    
+    for run = 1:6
+        if run == 1
+            impdir = strcat(fullfol,'/',epi1name); %strcat('C:/SweatStudy/Pilot_fMRI/EPI_files/Realigned');
+        elseif run == 2
+            impdir = strcat(fullfol,'/',epi2name); %strcat('C:/SweatStudy/Pilot_fMRI/EPI_files/Realigned');
+        elseif run == 3
+            impdir = strcat(fullfol,'/',epi3name);   
+        elseif run == 4
+            impdir = strcat(fullfol,'/',epi4name); %strcat('C:/SweatStudy/Pilot_fMRI/EPI_files/Realigned');
+        elseif run == 5
+            impdir = strcat(fullfol,'/',epi5name); %strcat('C:/SweatStudy/Pilot_fMRI/EPI_files/Realigned');
+        elseif run == 6
+            impdir = strcat(fullfol,'/',epi6name);      
+        else
+            impdir = strcat(fullfol,'/',epi7name);
+        end
+        
+        cd(impdir);
+        Filter = 'rp_asRMR';
+        rpfile = spm_select('FPList',impdir,Filter);
+        rps = importdata(rpfile);
+        %     rps = uiimport('-file');
+        %     rps = rps.rps;
+        %save ARF_sub
+        eval(['save OCF_sub' num2str(sub) '_epi' num2str(run) '_rp.txt rps']);
+        
+        diffs = [];
+        mocalc = [];
+        clist = [];
+        clistn = [];
+        
+        % Calculate the motion across images.  This finds the difference in the
+        % realignment parameters between each pair of images, corresponding to the
+        % amount of motion from one image to the next.  These values are placed
+        % into their own array.  The square root of the sum of squares is also
+        % caluclated and put into a separate array.  These two arrays are saved
+        % with the rps for the purpose of validation.
+        for ir = 1:length(rps)-1
+            diffs = [diffs; rps(ir+1,:) - rps(ir,:)];
+            mocalc = [mocalc; sqrt(diffs(ir,1).^2 + diffs(ir,2).^2 + diffs(ir,3).^2 + diffs(ir,4).^2 + diffs(ir,5).^2 + diffs(ir,6).^2)];
+        end
+        
+        % This finds the images that need to be censored.  The first part finds
+        % where the motion was high and the second accounts for the images
+        % surrounding the aberrant valume.
+        clist = find(mocalc > 2);
+        if ~isempty(clist)
+            for cs = 1:length(clist)
+                clistn = [clistn; clist(cs); clist(cs)+1; clist(cs)+2];
+            end
+        end
+        
+        % Save the motion parameters and censor list for later use in the model.
+        eval(['save mps_sub' num2str(sub) '_epi' num2str(run) ' rps diffs mocalc']);
+        eval(['save censorlist_sub' num2str(sub) '_epi' num2str(run) ' clistn']);
+        
+        figure; 
+        hold on; plot(rps(:,1),'k');
+        hold on; plot(rps(:,2),'b');
+        hold on; plot(rps(:,3),'g');
+        hold on; plot(rps(:,4),'c');
+        hold on; plot(rps(:,5),'m');
+        hold on; plot(rps(:,6),'y');
+        legend('X','Y','Z','r','?','?','Coordinate');
+        
+        title('Visual inspection');
+        
+        eval(['saveas(gcf,''OCF_sub' num2str(sub) '_epi' num2str(run) '_rp.jpeg'');']); %Save graph to an Illustrator file
+    end
+    
+end
+
+%% Visual inspection -- plot the motion rp
+
+% figure; plot
+% hold on; plot(rps(:,1),'k');
+% hold on; plot(rps(:,2),'b');
+% hold on; plot(rps(:,3),'g');
+% hold on; plot(rps(:,4),'c');
+% hold on; plot(rps(:,5),'m');
+% hold on; plot(rps(:,6),'y');
+% legend('X','Y','Z','r','?','?','Coordinate');
+% % figure; plot(horz, F45allresp, 'm'); hold on; plot(horz, F39allresp, 'r'); plot(horz, F33allresp, 'y'); plot(horz, F27allresp, 'c');  plot(horz, F21allresp, 'g'); plot(horz, F15allresp, 'b'); plot(horz, F2allresp, 'k');
+% % legend('45%','39%','33%','27%','21%','15%','2%','Location','NorthWest');
+% title('Visual inspection');
+% 
+% eval(['saveas(gcf,''ARF_sub' num2str(sub) '_block' num2str(run) '_rp.ai'');']); %Save graph to an Illustrator file  
+% eval(['saveas(gcf,''ARF_sub' num2str(sub) '_block' num2str(run) '_rp.ai'');']);
+
+
+%% STEP 5: Prepare Fieldmaps - FM1,2
 % Here the fieldmaps are prepared for the upcoming correction step.  In
 % order to use our fieldmaps we first need to makes the brain out.  This
 % calls for a skull-stripped mask, which can be accomplished with a
@@ -585,7 +706,7 @@ end
 % step.  The relevant dicoms should already be imported earlier.
 
 % Prepare the mask
-subs = 9;
+subs = 34;
 
 for i = 1:length(subs)
     subfol = strcat('sub',num2str(subs(i)));
@@ -607,6 +728,7 @@ for i = 1:length(subs)
         end
         
         SetDirectory = strcat(fullfol,'/',fm_name);
+        
         
         if subs(i) == 1 && run ==1
             Filter = '-0006'; %This is the image (water map) that we'll be segmenting to make the mask.  WE have tested using this image and it works wery well, and it's certain to properly be in line with the filedmap, while the T1 may be slightly off.
@@ -630,6 +752,34 @@ for i = 1:length(subs)
             Filter = '-0006';
         elseif subs(i) ==10 && run == 2
             Filter = '-0012';
+        elseif subs(i) ==10 && run == 3
+            Filter = '-0005';
+        elseif subs(i) ==10 && run == 4
+            Filter = '-0008';
+        elseif subs(i) ==15 && run == 4
+            Filter = '-0013';  
+        elseif subs(i) ==17 && run == 3
+            Filter = '-0008';
+        elseif subs(i) ==17 && run == 4      
+            Filter = '-0011';  
+        elseif subs(i) ==18 && run == 3
+            Filter = '-0008';
+        elseif subs(i) ==18 && run == 4
+            Filter = '-0011';           
+        elseif subs(i) ==19 && run == 1
+            Filter = '-0005';     
+        elseif subs(i) ==20 && run == 4
+            Filter = '-0006';    
+        elseif subs(i) ==24 && run == 3
+            Filter = '-0005';
+        elseif subs(i) ==24 && run == 4
+            Filter = '-0008';  
+        elseif subs(i) ==28 && run == 1
+            Filter = '-0006';
+        elseif subs(i) ==28 && run == 2
+            Filter = '-0013';    
+        elseif subs(i) ==29 && run == 2
+            Filter = '-0013';            
         end
         
         watim = spm_select('FPList',SetDirectory,Filter);
@@ -788,6 +938,238 @@ for i = 1:length(subs)
     
 end
 
+%% STEP 5b: Prepare Fieldmaps - FM3,4
+% Here the fieldmaps are prepared for the upcoming correction step.  In
+% order to use our fieldmaps we first need to makes the brain out.  This
+% calls for a skull-stripped mask, which can be accomplished with a
+% segmented water-based image, helpfully output by the fieldmap collection
+% step.  The relevant dicoms should already be imported earlier.
+
+% Prepare the mask
+subs = 33%[17:18 20 22:31];
+
+for i = 1:length(subs)
+    subfol = strcat('sub',num2str(subs(i)));
+    fullfol = strcat(mainpath,'/',subfol);
+    for run = 3:4
+        switch run
+            case 1
+                fm_name = fm1name;
+                Filter = '-0005';
+            case 2
+                fm_name = fm2name;
+                Filter = '-0011';
+            case 3
+                fm_name = fm3name;
+                Filter = '-0006';
+            case 4
+                fm_name = fm4name;
+                Filter = '-0009';
+        end
+        
+        SetDirectory = strcat(fullfol,'/',fm_name);
+        
+        if subs(i) == 1 && run ==1
+            Filter = '-0006'; %This is the image (water map) that we'll be segmenting to make the mask.  WE have tested using this image and it works wery well, and it's certain to properly be in line with the filedmap, while the T1 may be slightly off.
+        elseif subs(i) ==1 && run == 3
+            Filter = '-0007';
+        elseif subs(i) ==1 && run == 4
+            Filter = '-0013';
+        elseif subs(i) ==2 && run == 1
+            Filter = '-0007';
+        elseif subs(i) ==2 && run == 2
+            Filter = '-0013';
+        elseif subs(i) ==2 && run == 3
+            Filter = '-0007';
+        elseif subs(i) ==2 && run == 4
+            Filter = '-0010';
+        elseif subs(i) ==3 && run == 2
+            Filter = '-0012';
+        elseif subs(i) ==4 && run == 2
+            Filter = '-0012';
+        elseif subs(i) ==10 && run == 1
+            Filter = '-0006';
+        elseif subs(i) ==10 && run == 2
+            Filter = '-0012';
+        elseif subs(i) ==10 && run == 3
+            Filter = '-0005';
+        elseif subs(i) ==10 && run == 4
+            Filter = '-0008';
+        elseif subs(i) ==15 && run == 4
+            Filter = '-0013';  
+        elseif subs(i) ==17 && run == 3
+            Filter = '-0008';
+        elseif subs(i) ==17 && run == 4      
+            Filter = '-0011';  
+        elseif subs(i) ==18 && run == 3
+            Filter = '-0008';
+        elseif subs(i) ==18 && run == 4
+            Filter = '-0011';           
+        elseif subs(i) ==19 && run == 1
+            Filter = '-0005';     
+        elseif subs(i) ==20 && run == 4
+            Filter = '-0006';    
+        elseif subs(i) ==24 && run == 3
+            Filter = '-0005';
+        elseif subs(i) ==24 && run == 4
+            Filter = '-0008';            
+        end
+        
+        watim = spm_select('FPList',SetDirectory,Filter);
+        watim2 = [];
+        mm = size(watim(1,:),2);
+        
+        for crct = 1:size(watim,1)
+            if strcmp(watim(crct,mm-2:mm),'img') == 1
+                watim2 = [watim2; watim(crct,:)];
+            else
+            end
+        end
+        
+        watim = watim2;
+        cd(SetDirectory);
+        
+        % The first structure makes sure we get grey matter, white
+        % matter, and CSF probability maps, important for making a
+        % complete mask.  The second is just the default inputs for
+        % image segcmentation.
+        output.GM = [0 0 1];
+        output.WM = [0 0 1];
+        output.CSF = [0 0 1];
+        output.biascor = 1;
+        output.cleanup = 0;
+        opts.tpm = {
+            %                     'C:/Program Files/MATLAB/Added_Toolboxes/spm8/spm8/tpm/grey.nii'   % change the directory to your own directory
+            %                     'C:/Program Files/MATLAB/Added_Toolboxes/spm8/spm8/tpm/white.nii'  % Eg: mine is C:/Users/YAN/Desktop/Hope's ducuments/Hope files/Apps/Research Software/spm8/tpm/white.nii
+            %                     'C:/Program Files/MATLAB/Added_Toolboxes/spm8/spm8/tpm/csf.nii'
+            '/Volumes/Elements/OCF/tpm/grey.nii'
+            '/Volumes/Elements/OCF/tpm/white.nii'
+            '/Volumes/Elements/OCF/tpm/csf.nii'
+            };
+        opts.ngaus = [2
+            2
+            2
+            4];
+        opts.regtype = 'mni';
+        opts.warpreg = 1;
+        opts.warpco = 25;
+        opts.biasreg = 0.0001;
+        opts.biasfwhm = 60;
+        opts.samp = 3;
+        opts.msk = {''};
+        
+        opts2.cleanup = 0;
+        opts2.biascor = 1;
+        opts2.GM = [0 0 1];
+        opts2.WM = [0 0 1];
+        opts2.CSF = [0 0 1];
+        
+        % This is the function that actually does segmentation.
+        % Despite being labeled suspiciously like "preprocess" it
+        % is only the segmentation step.
+        segmath = spm_preproc(watim,output,opts);
+        [sn_a,sn_b] = spm_prep2sn(segmath);
+        spm_preproc_write(sn_a,opts2);
+        
+        
+        % Now grab the segmented images and use ImCalc to create
+        % the mask.
+        SetDirectory = strcat(fullfol,'/',fm_name);
+        Filter2 = 'c*';
+        
+        csegs = spm_select('FPList',SetDirectory,Filter2);
+        csegs2 = [];
+        mm = size(csegs(1,:),2);
+        
+        for crct = 1:size(csegs,1)
+            if strcmp(csegs(crct,mm-2:mm),'img') == 1
+                csegs2 = [csegs2; csegs(crct,:)];
+            else
+            end
+        end
+        
+        csegs = csegs2;
+        csegsdat = spm_vol(csegs);
+        
+        % To choose the name of your output, make sure to have this
+        % set.
+        
+        info = spm_vol(csegs(1,:));
+        info.fname = 'fm_mask.img';
+        
+        % These are parameters for doing the image calculation.
+        % You shouldn't need to change any of them.
+        mkflag.dmtx = 0;
+        mkflag.mask = 0;
+        mkflag.hold = 4;
+        
+        mflag = {0;0;4};
+        
+        % The threshold of 0.2 was determined from testing.  It may
+        % be a good idea to look at the masks for multiple subjects
+        % and vary the threshold a bit to see what's best.
+        spm_imcalc(csegsdat,info,'(i1+i2+i3)>0.2',mflag);
+        
+        % Finally we will mask the fieldmaps.  We must grab both
+        % the mask and the fieldmap image.
+        
+        SetDirectory = strcat(fullfol,'/',fm_name);
+        
+        Filter3 = num2str(abs(str2num(Filter)*100)+1);% the fieldmap image No. is 100*water image No. + 1
+         
+        orig1 = spm_select('FPList',SetDirectory,Filter3);
+        orig1a = [];
+        mm = size(orig1(1,:),2);
+        
+        for crct = 1:size(orig1,1)
+            if strcmp(orig1(crct,mm-2:mm),'img') == 1
+                orig1a = [orig1a; orig1(crct,:)];
+            else
+            end
+        end
+        
+        orig1 = orig1a;
+        
+        Filter4 = 'mask';
+        
+        orig2 = spm_select('FPList',SetDirectory,Filter4);
+        orig2a = [];
+        mm = size(orig2(1,:),2);
+        
+        for crct = 1:size(orig2,1)
+            if strcmp(orig2(crct,mm-2:mm),'img') == 1
+                orig2a = [orig2a; orig2(crct,:)];
+            else
+            end
+        end
+        
+        orig2 = orig2a;
+        
+        orig = [spm_vol(orig1);spm_vol(orig2)];
+        
+        fflag.dmtx = 0;
+        fflag.mask = 0;
+        fflag.hold = 4;
+        
+        fmflag = {0;0;4};
+        
+        info2 = spm_vol(orig1);
+        info2.fname = 'fpm_maskedFM.img';
+        
+        % Order is important in this equation.  Here, the binary
+        % mask is being made with the mask file, because the mask
+        % file is the second image in the input array.  That mask
+        % os then being applied to the first image, the fieldmap.
+        % If you switch around the order you won't get a fieldmap
+        % output, so be careful!  If you input the mask as the
+        % first image, then you would have to switch the values.
+        % Let's just keep it consistent here for everyone's safety.
+        spm_imcalc(orig,info2,'i1.* (i2>0)',fmflag);
+        
+        
+    end
+    
+end
 %% Fieldmap correction
 % need to add /apps/spm8-5236/noarch/spm8/toolbox/FieldMap  to path
 
@@ -795,7 +1177,7 @@ end
 
 mainpath = '/Volumes/Elements/OCF';
 
-subs = 1;
+subs = 3:16;
 
 shortTE = 7;
 longTE = 10;
